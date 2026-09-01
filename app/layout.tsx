486a8e31566dd210b3ae728f6b44c0bf1e1d0eba
sara-kjellemo <sara@genomain.com>
2026-09-01 12:57:16 +0200

    fixed images and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -123,6 +123,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400&family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400&display=swap" rel="stylesheet" />
         <meta name="geo.region" content="SE-O" />
         <meta name="geo.placename" content="Smögen, Sotenäs, Sweden" />
         <meta name="geo.position" content="58.3536;11.2281" />
